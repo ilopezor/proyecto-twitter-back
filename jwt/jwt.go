9f8b8cba9d26a8014e225c76ecf3c4d72d2936cb
ilopezor <isaaclopez281@gmail.com>
2021-12-28 00:34:34 -0500

    se sube login

--- jwt/jwt.go
+++ jwt/jwt.go
@@ -10,7 +10,7 @@ import (
 /*GeneroJWT genera el encriptado con JWT */
 func GeneroJWT(t models.Usuario) (string, error) {
 
-	miClave := []byte("MastersdelDesarrollo")
+	miClave := []byte("proyectobackgo")
 
 	payload := jwt.MapClaims{
 		"email":            t.Email,
